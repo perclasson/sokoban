b918da122d73fafbc3cc193ff52be5e0b2a58690
Peter Nycander <peter.nycander@gmail.com>
2013-09-18 16:56:52 +0200

    added stuck check

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
@@ -10,30 +11,46 @@ public class Main {
 		new Main();
 	}
 
-	private ArrayList<String> board;
-	private char[][] path;
+	private char[][] board;
 	
 	public Main() {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		board = new ArrayList<String>();
+		List<String> tmpBoard = new ArrayList<String>();
 		int matrixSize = 0;
 		String line = "";
 
 		try {
 			while((line = in.readLine()) != null) {
-				board.add(line);
+				tmpBoard.add(line);
 				matrixSize = Math.max(matrixSize, line.length());
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		matrixSize = Math.max(matrixSize, board.size());
-		path = new char[matrixSize][matrixSize];
+		matrixSize = Math.max(matrixSize, tmpBoard.size());
+		board = new char[matrixSize][matrixSize];
+		
+		for(int i = 0 ; i < tmpBoard.size() ; i++) {
+			board[i] = tmpBoard.get(i).toCharArray();
+		}
+		
 		System.out.println(findPath());
 	}
 
 	private String findPath() {
 		return null;
+	}
+	
+	private boolean freeSpace(int x, int y) {
+		return board[x][y] != ' ' && board[x][y] != '.';
+	}
+	
+	private boolean isStuck(int x, int y) {
+		if ((freeSpace(x-1, y) && freeSpace(x+1, y)) ||
+				(freeSpace(x, y-1) && freeSpace(x, y+1))) {
+			return false;
+		}
+		return true;
 	}
 
 }
