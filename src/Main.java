9e0c9807bc951c158b8b3174af8f9e97bf569233
Per Classon <perwclasson@gmail.com>
2013-09-19 14:00:38 +0200

    Read board level from file

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,78 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Main {
-
+	private static final Boolean TEST = true;
+	
 	public static void main(String[] args) {
 		new Main();
 	}
 
 	private char[][] board;
-	
-	public Main() {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		List<String> tmpBoard = new ArrayList<String>();
-		int matrixSize = 0;
-		String line = "";
 
-		try {
-			while((line = in.readLine()) != null) {
-				tmpBoard.add(line);
-				matrixSize = Math.max(matrixSize, line.length());
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		matrixSize = Math.max(matrixSize, tmpBoard.size());
-		board = new char[matrixSize][matrixSize];
-		
-		for(int i = 0 ; i < tmpBoard.size() ; i++) {
+	public Main() {
+		BufferedReader in = getBufferedReader();
+		List<String> tmpBoard = readBoard(in);
+
+		board = new char[tmpBoard.size()][];
+
+		for (int i = 0; i < tmpBoard.size(); i++) {
 			board[i] = tmpBoard.get(i).toCharArray();
 		}
-		
+
 		System.out.println(findPath());
 	}
 
 	private String findPath() {
 		return null;
 	}
-	
+
 	private boolean freeSpace(int x, int y) {
 		return board[x][y] != ' ' && board[x][y] != '.' && board[x][y] != '@';
 	}
-	
+
 	private boolean isStuck(int x, int y) {
-		if ((freeSpace(x-1, y) && freeSpace(x+1, y)) ||
-				(freeSpace(x, y-1) && freeSpace(x, y+1))) {
+		if ((freeSpace(x - 1, y) && freeSpace(x + 1, y))
+				|| (freeSpace(x, y - 1) && freeSpace(x, y + 1))) {
 			return false;
 		}
 		return true;
+	}
+
+	private BufferedReader getBufferedReader() {
+		if (TEST) {
+			try {
+				return new BufferedReader(new FileReader("data/all-slc"));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		return new BufferedReader(new InputStreamReader(System.in));
+	}
+
+	private List<String> readBoard(BufferedReader in) {
+		List<String> board = new ArrayList<String>();
+		String line = null;
+		
+		try {
+			if (TEST) {
+				// Get the level name
+				line = in.readLine();
+			}
+			// We read until we get a new LEVEL or we have reached the end of file
+			while ((line = in.readLine()) != null && line.charAt(0) != ';') {
+				board.add(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return board;
 	}
 
 }
