d6e710bba199ed4123ba19125bb71c6b7178f020
Peter Nycander <peter.nycander@gmail.com>
2013-09-25 17:31:46 +0200

    Fixed hashCode of GameState and ensures internal ordering of boxlist

--- src/BoxList.java
+++ src/BoxList.java
@@ -1,44 +1,54 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 
 
 public class BoxList {
-	private static final int X = 0, Y = 1, UP = 2, RIGHT = 3, DOWN = 4, LEFT = 5;
-	private List<int[]> boxes;
+	public static final int X = 0, Y = 1, UP = 2, RIGHT = 3, DOWN = 4, LEFT = 5;
+	private Map<Integer, int[]> boxes;
+	public static final int X_SHIFT = 10000;
 	
 	public BoxList() {
-		boxes = new ArrayList<int[]>();
+		boxes = new TreeMap<Integer, int[]>();
 	}
 	
 	public void addBox(int x, int y) {
 		addBox(x, y, 0, 0, 0, 0);
 	}
 	
+	public Set<Entry<Integer, int[]>> getEntrySet() {
+		return boxes.entrySet();
+	}
+	
 	public void addBox(int x, int y, int up, int right, int down, int left) {
 		int[] box = {x, y, up, right, down, left};
-		boxes.add(box);
+		boxes.put(x*X_SHIFT+y,box);
 	}
 	
 	public int size() {
 		return boxes.size();
 	}
 	
-	public int getX(int box) {
+	public int getX(String box) {
 		return boxes.get(box)[X];
 	}
-	public int getY(int box) {
+	public int getY(String box) {
 		return boxes.get(box)[Y];
 	}
-	public int getUp(int box) {
+	public int getUp(String box) {
 		return boxes.get(box)[UP];
 	}
-	public int getRight(int box) {
+	public int getRight(String box) {
 		return boxes.get(box)[RIGHT];
 	}
-	public int getDown(int box) {
+	public int getDown(String box) {
 		return boxes.get(box)[DOWN];
 	}
-	public int getLeft(int box) {
+	public int getLeft(String box) {
 		return boxes.get(box)[LEFT];
+	}
+	public boolean containsBox(int x, int y) {
+		return boxes.containsKey(x*X_SHIFT+y);
 	}
 }
--- src/GameState.java
+++ src/GameState.java
@@ -1,19 +1,34 @@
+import java.util.Map.Entry;
+
 
 public class GameState {
 	private BoxList boxList;
 	public int x, y;
+	private String path;
 	
 	public GameState(BoxList boxList) {
 		this.boxList = boxList;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	public boolean containsBox(int x, int y) {
+		return boxList.containsBox(x, y);
 	}
 	
 	@Override
 	public int hashCode() {
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0 ; i < boxList.size() ; i++) {
-			sb.append(boxList.getX(i)).append(',').append(boxList.getY(i)).append(' ');
-		}
+		for (Entry<Integer, int[]> e : boxList.getEntrySet())
+			sb.append(e.getValue()[BoxList.X]).append(',').append(e.getValue()[BoxList.Y]).append(' ');
 		return sb.toString().hashCode();
 	}
+
 	
 }
