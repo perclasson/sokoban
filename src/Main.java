c29da53c840576abd0057e2fc7ca4cc3bdddc78a
Per Classon <perwclasson@gmail.com>
2013-09-26 16:17:53 +0200

    Find deadlocks

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
 	public static final char PLAYER_ON_GOAL = '+';
 	public static final char BOX = '$';
 	public static final char BOX_ON_GOAL = '*';
+	public static final char DEADLOCK = 'D';
 	public static final int[] dx = { -1, 1, 0, 0 };
 	public static final int[] dy = { 0, 0, -1, 1 };
 	public static final int[] bigdx = { -1, -1, -1, 0, 0, 1, 1, 1, 0 };
@@ -81,6 +82,97 @@ public class Main {
 				}
 			}
 		}
+
+		int upperLeftX = -1, downLeftX = -1, maxWidth = 0;
+
+		// Mark horizontal deadlocks
+		for (int y = 0; y < board.length; y++) {
+			maxWidth = Math.max(board[y].length, maxWidth);
+			for (int x = 0; x < board[y].length; x++) {
+				try {
+					if (upperLeftX > 0) {
+						if (board[y][x] == WALL && board[y + 1][x] == WALL) {
+							for (int xi = upperLeftX; xi < x; xi++) {
+								board[y + 1][xi] = DEADLOCK;
+							}
+							upperLeftX = -1;
+
+						} else if (board[y][x] != WALL || (board[y + 1][x] != SPACE && board[y + 1][x] != DEADLOCK)) {
+							upperLeftX = -1;
+						}
+					} else if (downLeftX > 0) {
+						if (board[y][x] == WALL && board[y + 1][x] == WALL) {
+							for (int xi = downLeftX; xi < x; xi++) {
+								board[y][xi] = DEADLOCK;
+							}
+							downLeftX = -1;
+
+						} else if ((board[y][x] != SPACE && board[y][x] != DEADLOCK) || board[y + 1][x] != WALL) {
+							downLeftX = -1;
+						}
+					} else if (board[y][x] == WALL && board[y + 1][x] == WALL && board[y][x + 1] == WALL && (board[y + 1][x + 1] == SPACE || board[y + 1][x + 1] == DEADLOCK)) {
+						// We found a corner
+						board[y + 1][x + 1] = DEADLOCK;
+						upperLeftX = x + 1;
+					} else if (board[y][x] == WALL && board[y + 1][x] == WALL && (board[y][x + 1] == SPACE || board[y][x + 1] == DEADLOCK) && board[y + 1][x + 1] == WALL) {
+						// We found a corner
+						board[y][x + 1] = DEADLOCK;
+						downLeftX = x + 1;
+					} else if ((board[y][x] == SPACE || board[y][x] == DEADLOCK) && board[y+1][x] == WALL && board[y][x+1] == WALL && board[y+1][x+1] == WALL) {
+						// We found a corner
+						board[y][x] = DEADLOCK;
+					}
+				} catch (ArrayIndexOutOfBoundsException e) {
+					continue;
+				}
+			}
+			upperLeftX = -1;
+			downLeftX = -1;
+		}
+
+		int upperLeftY = -1, upperRightY = -1;
+
+		// Mark vertical deadlocks
+		for (int x = 0; x < maxWidth; x++) {
+			for (int y = 0; y < board.length; y++) {
+				try {
+					if (upperLeftY > 0) {
+						if (board[y][x] == WALL && board[y][x + 1] == WALL) {
+							for (int yi = upperLeftY; yi < y; yi++) {
+								board[yi][x + 1] = DEADLOCK;
+							}
+							upperLeftY = -1;
+
+						} else if (board[y][x] != WALL || (board[y][x + 1] != SPACE && board[y][x + 1] != DEADLOCK)) {
+							upperLeftY = -1;
+						}
+					} else if (upperRightY > 0) {
+						if (board[y][x] == WALL && board[y][x + 1] == WALL) {
+							for (int yi = upperRightY; yi < y; yi++) {
+								board[yi][x] = DEADLOCK;
+							}
+							upperRightY = -1;
+
+						} else if ((board[y][x] != SPACE && board[y][x] != DEADLOCK) || board[y][x + 1] != WALL) {
+							upperRightY = -1;
+						}
+					} else if (board[y][x] == WALL && board[y + 1][x] == WALL && board[y][x + 1] == WALL && (board[y + 1][x + 1] == SPACE || board[y + 1][x + 1] == DEADLOCK)) {
+						// We found a corner
+						board[y + 1][x + 1] = DEADLOCK;
+						upperLeftY = y + 1;
+					} else if (board[y][x] == WALL && (board[y + 1][x] == SPACE || board[y + 1][x] == DEADLOCK) && board[y][x + 1] == WALL && board[y + 1][x + 1] == WALL) {
+						// We found a corner
+						board[y + 1][x] = DEADLOCK;
+						upperRightY = y + 1;
+					}
+				} catch (ArrayIndexOutOfBoundsException e) {
+					continue;
+				}
+			}
+			upperLeftY = -1;
+			upperRightY = -1;
+		}
+		
 		return new GameState(bl, playerX, playerY);
 	}
 
@@ -166,6 +258,8 @@ public class Main {
 	private boolean isDeadlock(GameState state, int bx, int by) {
 		if (board[by][bx] == GOAL)
 			return false;
+		else if (board[by][bx] == DEADLOCK)
+			return true;
 		for (int i = 0; i < 9; i++) {
 			if (!isStuck(state, bx + bigdx[i], by + bigdy[i])) {
 				return false;
