2672381f64730c1019c957a299c4ca3ef2bd5e6f
tobiasandersson <tobias.ason@gmail.com>
2013-10-16 12:27:49 +0200

    improved tester

--- src/Tester.java
+++ src/Tester.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Tester {
@@ -12,42 +7,60 @@ public class Tester {
 	private static Scanner scanner;
 
 	public static void main(String[] args) throws FileNotFoundException {
-		boolean verbose = false;
-		boolean fail = false;
+		boolean verbose = true;
+		boolean fail = true;
 		String source = null;
+		boolean chooseLevel = false;
+		String chosenLevel = "";
 		for (int i = 0; i < args.length; i++) { // verbose - Print successful maps
-			if (args[i].equals("-v")) {
-				verbose = true;
-			} else if (args[i].equals("-f")) { // stop if one test fails
-				fail = true;
-			} else {
-				source = args[i];
+			if (args[i].equals("-s")) {
+				verbose = false;
+			} else if (args[i].equals("-c")) { // continue even if a test fails
+				fail = false;
+			} else if(args[i].equals("-l")){ // choose level
+				chooseLevel = true;
+				chosenLevel = args[i+1];
+			} else if(args[i].equals("-f")){ // source file
+				source = args[i+1];
 			}
 		}
 		scanner = new Scanner(new File(source));
-		scanner.useDelimiter(";");
+		scanner.useDelimiter(";LEVEL ");
+		int currentLevel = 0;
 		while (scanner.hasNext()) {
+			currentLevel++;
 			String both = scanner.next();
-			String level = both.split("\n")[0];
 			String map = both.split("[0-9]+")[1];
+
+			if(chooseLevel && currentLevel != Integer.parseInt(chosenLevel)) {
+				continue;
+			}
+			
 			if (verbose) {
-				System.out.println(level);
+				System.out.println("LEVEL: "+currentLevel);
 				System.out.println(map);
 			}
+			
 			Main main = new Main(makeBoard(map));
+			if(verbose) {
+				System.out.println("Solving...");
+			}
 			String path = main.solve();
 			if(verbose) {
-				System.out.println(path);
+				System.out.println("Finished! Path: " + path);
 			}
 			if(path.equals("No path")) {
 				System.out.println();
 				System.out.println("FFFFFAAAAAAAIIIIIIIIILLLLLL");
-				System.out.println(level);
-				System.err.println("MAP:");
+				System.out.println(currentLevel);
+				System.out.println("MAP:");
 				System.out.println(map);
 				if(fail) {
 					break;
 				}
+			}
+			if(chooseLevel) {
+				System.exit(0);
 			}
 		}
 
